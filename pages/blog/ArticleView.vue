7b9b56a06ebb06d01ebcec99fe3c7596b514c018
Godwin peter. O <me@godwin.dev>
2024-04-06 00:23:10 +0100

    create pages

--- pages/blog/ArticleView.vue
+++ pages/blog/ArticleView.vue
@@ -1,6 +1,6 @@
-<script setup lang='ts'>
-const route = useRoute()
-const slug = route.params.slug.toString()
+<script setup lang="ts">
+const route = useRoute();
+const slug = route.params.slug.toString();
 </script>
 
 <template>
@@ -9,18 +9,14 @@ const slug = route.params.slug.toString()
       <template #default="{ doc }">
         <h1>{{ doc.title }}</h1>
         <p>{{ doc.description }}</p>
-        <p class="font-bold">
-          Author: {{ doc.author }}
-        </p>
-        <hr>
+        <p class="font-bold">Author: {{ doc.author }}</p>
+        <hr />
         <ContentRenderer :value="doc" />
       </template>
       <!-- Slot if document is not found -->
       <template #not-found>
-        <h1 class="text-2xl">
-          Content Page ({{ slug }}) not found
-        </h1>
+        <h1 class="text-2xl">Content Page ({{ slug }}) not found</h1>
       </template>
-    </contentdoc>
+    </ContentDoc>
   </div>
 </template>
